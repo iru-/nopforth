b6caf0be77556c5383e03521933e56227131c6d0
Iruatã Souza <iru.muzgo@gmail.com>
2020-05-11 18:37:08 +0200

    [linux] align return stack on 16 bytes before OS calls to comply with the ABI

--- src/linux/os.s
+++ src/linux/os.s
@@ -1,58 +1,87 @@
     .text
 
+# System V x86-64 ABI requires a stack aligned on a 16 bytes boundary.
+# The prolog saves the return stack pointer on rbx since the latter must be
+# saved by the callee, and so is guaranteed to be preserved across the call.
+# Lastly, the prolog forces the stack alignment. The epilog thus restores
+# the return stack pointer, which may or may not be aligned.
+.macro prolog
+    mov %rsp, %rbx
+    and $-16, %rsp
+.endm
+
+.macro epilog
+    mov %rbx, %rsp
+.endm
+
+
 sysread:
+    prolog
     mov %rax, %rdi
     drop_
     mov %rax, %rdx
     drop_
     mov %rax, %rsi
     call read@plt
+    epilog
     ret
 
 syswrite:
+    prolog
     mov %rax, %rdi
     drop_
     mov %rax, %rdx
     drop_
     mov %rax, %rsi
     call write@plt
+    epilog
     ret
 
 sysexit:
+    prolog
     mov %rax, %rdi
     call exit@plt
 
 sysopen:
+    prolog
     mov %rax, %rdx
     drop_
     mov %rax, %rsi
     drop_
     mov %rax, %rdi
     call open@plt
+    epilog
     ret
 
 syscreate:
+    prolog
     mov %rax, %rsi
     drop_
     mov %rax, %rdi
     call creat@plt
+    epilog
     ret
 
 sysclose:
+    prolog
     mov %rax, %rdi
     call close@plt
+    epilog
     ret
 
 sysseek:
+    prolog
     mov %rax, %rdi
     drop_
     mov %rax, %rdx
     drop_
     mov %rax, %rsi
     call lseek@plt
+    epilog
     ret
 
 sysmmap:
+    prolog
     mov %rax, %r9
     drop_
     mov %rax, %r8
@@ -65,13 +94,16 @@ sysmmap:
     drop_
     mov %rax, %rdi
     call mmap@plt
+    epilog
     ret
 
 sysmunmap:
+    prolog
     mov %rax, %rsi
     drop_
     mov %rax, %rdi
     call munmap@plt
+    epilog
     ret
 
 sys6:
@@ -103,29 +135,39 @@ syscall2:  push %rax; drop_; jmp sys2
 syscall1:  push %rax; drop_; jmp sys1
 
 _dlopen:
+    prolog
     mov %rax, %rsi
     drop_
     mov %rax, %rdi
     call dlopen@plt
+    epilog
     ret
 
 _dlsym:
+    prolog
     mov %rax, %rsi
     drop_
     mov %rax, %rdi
     call dlsym@plt
+    epilog
     ret
 
 _dlclose:
+    prolog
     mov %rax, %rdi
     call dlclose@plt
+    epilog
     ret
 
 _dlerror:
+    prolog
     call dlerror@plt
+    epilog
     ret
 
 _getenv:
+    prolog
     mov %rax, %rdi
     call getenv@plt
+    epilog
     ret
