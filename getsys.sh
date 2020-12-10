8f4b7b3963bc5bc7631f69ee0ba78ca84c8f4435
Iruatã Souza <iru.muzgo@gmail.com>
2020-12-10 11:30:36 +0100

    getsys.sh: use uname -s instead of -o

--- getsys.sh
+++ getsys.sh
@@ -2,12 +2,4 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2018-2020 Iruatã Martins dos Santos Souza
 
-sys=$(uname -o | tr A-Z a-z)
-case $sys in
-gnu/linux)
-	echo linux
-	;;
-*)
-	echo $sys
-	;;
-esac
+uname -s | tr A-Z a-z
